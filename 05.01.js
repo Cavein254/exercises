f42f56a13494e99e9e62385f0d610adc783c9b1d
johnmuthua <caveomuthama@gmail.com>
2022-09-23 21:45:57 +0300

    simple sort

--- 05.01.js
+++ 05.01.js
@@ -1,10 +1,33 @@
-const arr = [2, 3, 4, 5, 22, 5];
-let tempArr = arr;
-let minMaxArr = [];
-for (let i = 0; i < arr.length; i++) {
-  let min = Math.min(...tempArr);
-  tempArr = tempArr.filter((item) => item !== min);
-  minMaxArr.push(min);
+const arr = [2, 13, 4, -75, 22, 5];
+
+function maxMin(arr) {
+  let tempArr = arr;
+  let maxMinArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let max = Math.max(...tempArr);
+    tempArr = tempArr.filter((item) => item !== max);
+    maxMinArr.push(max);
+  }
+  maxMinArr.pop();
+  console.log(maxMinArr);
+  return maxMinArr;
 }
-minMaxArr.pop();
-console.log(minMaxArr);
+
+// console.log(arr.sort((a, b) => a - b));
+console.log(arr.sort());
+
+const arr2 = ["father", "mother", "children", "adam", "eve"];
+console.log(arr2.sort());
+
+const arr3 = ["Eve", "adam"];
+console.log(
+  arr3.sort((a, b) => a.toLowerCase().localeCompare(b.toLowercase))
+  //   a.toLowerCase().localeCompare(b.toLowerCase());
+);
+console.log(arr3.sort(Intl.Collator().compare));
+function smallToLarge(arr) {
+  return arr.sort((a, b) => a - b);
+}
+
+console.log("-------");
+console.log(smallToLarge(arr));
